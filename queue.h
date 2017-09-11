f3e0e7576344126bac5f941eb9cd076214aa592d
Johannes Spies <jojos.mobil@gmail.com>
2017-09-11 13:16:26 +0200

    Added description

--- queue.h
+++ queue.h
@@ -1,13 +1,130 @@
 #if !defined(QUEUE_H)
 /* ========================================================================
-   $File: $
-   $Date: $
-   $Revision: $
+   $File: queue.h $
+   $Date: 11.09.2017 (DD/MM/YYYY) $
+   $Revision: 1 $
    $Creator: Johannes Spies $
    $Notice: (C) Copyright 2017 by Johannes Spies. All Rights Reserved. $
 
-   Simple one-header-'library' for a single-link(-ed) queue for C++ using
-   templates.
+   /=======\
+   |QUEUE.H|
+   \=======/
+
+   TL;DR:  Simple one-header-'library' for a single-link(-ed) queue for C++
+   ------  using templates. It exists because I don't want to write code
+           for using queues and other simple data structures everytime I
+           code... Maybe there are coming some more simple libraries if this
+           is going to be used frequently by my in my code -- but, who 
+           knows, maybe this library will just be some stupid bullshit...
+           IDK, yet.
+
+   Short TL;DR: Queue-library for C++.
+   ------------
+   
+   Note:   The library is not designed to be save -> There can be SIGSEGVs
+   -----   and other errors at any time. There is _absolutly_ no warranty
+           shipped with this piece of code. I don't know what it does and
+           I don't know what it _may_ do to your computer.
+
+   Usage:
+   ======
+   Include:
+   --------
+       The library can be included in every file, BUT: There is no 
+       implementation (or reference) to any function unless you put
+
+           #define QUEUE_IMPLEMENTAION
+   
+       BEFORE one
+
+           #include "queue.h"
+       
+       . There are some more macros you can define to change the library
+       behavior. _Some_ of them are: 
+          - QUEUE_DEBUG: If this macro is defined (BEFORE including the
+            library in _one_ compilation unit), there will be _some_
+            _few_ debug checks combined with debug output in the library.
+            This only works if there is stdio.h available (and NOT
+            QUEUE_NO_STDIO defined, which leads to the next macro...).
+          - QUEUE_NO_STDIO: If this macro is defined, the library knows
+            that it is not allowed to include stdio.h as a dependency.
+          - QUEUE_NO_MALLOC: If this macor is defined, the library knows
+            that it is not allowed to include malloc.h. Further you are
+            expected to provide another allocator for the library since
+            it does not know what memory to use to (de)allocate new/old
+            elements. You can either set a entire new DefaultAllocator
+            or provide your own allocator when you are using allocation-
+            using library-functions.
+
+   Visible structures:
+   -----------
+   1. queue:           The representation of the data structure queue. 
+                       Should be self-explanatory.
+   2. queue-allocator: An allocator used by every queue. Can be set in
+                       function that require allocations/dynamic memory
+                       access (QueueAdd allocates bytes, size is size of
+                       an struct-pointer + size of the contained element;
+                       QueuePop deallocated previously aquired memory-
+                       bytes requested by QueueAdd). You can set your own
+                       malloc/free functions or pass a custom allocator to
+                       functions (mentioned before) which take in an 
+                       allocator as a _hidden_ parameter.
+
+   Functions:
+   ----------
+   1. CreateQueue:  Create a new, empty queue to work with
+                    Example:
+                    --------
+                        // Create a new empty queue
+                        queue<data_type> Queue = CreateQueue<data_type>();
+
+   2. QueueAdd:     Add elements (of type contained) to the queue
+                    Example:
+                    --------
+                        queue<data_type> Queue = ...;
+                        data_type NewElement = ...;
+                        QueueAdd(&Queue, NewElement);
+
+   3. QueueFront:   Get the frontmost element of the queue.
+
+   4. QueuePop:     Pop the frontmost element of the queue (the function
+                    does not return the _old_ content -> Call QueueFront
+                    first)
+
+   5. QueueCount:   Count all emenents in the queue.
+
+   6. QueueGet:     Get a pointer (!) to the element at the given index.
+
+   7. QueueForEach: Iterate over the elements in the queue -> Get a
+                    callback for each element visited. Return the element
+                    or a new element -> You can edit the content of an
+                    element while the for loop is stil ongoing. You can
+                    (and should) use the provided macro to 'generate'
+                    functions that can be called as for-each functions.
+                    Example:
+                    --------
+                        QUEUE_FOR_EACH_FUNCTION(SomeFunctionName, data_type)
+                        {
+                            // ...
+                            // Use the pointer to the element (Element) or
+                            // the index of the element (Index) (or don't).
+                            return(*Element);
+                        }
+                        
+                        // ...
+                        queue<data_type> Queue = ...
+                        QueueForEach(&Queue, &SomeFunctionName);
+   TODOs(js):
+   ----------
+   1. Maybe make C++s' lambdas working with this library. 
+   2. Handle/show errors if there is no std-lib or malloc.h.
+
+   Long time story short: You can use this library pretty simple.
+   That's possibly it.
+   Have fun using it, let my know if someone other than me uses it, 
+   go ahead, use it.
+
+   ~js
    ======================================================================== */
 #if defined(QUEUE_DEBUG) && !defined(QUEUE_NO_STDIO)
 #define QueueAssert(Expression) if(!(Expression)) {fprintf(stdout, "Expression %s in queue failed.\n", # Expression);}
